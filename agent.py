c67b258f6da1199eb16613d4d546b0949dbc92d7
Gitsamshi <shizhanszh34@gmail.com>
2025-06-12 00:11:50 -0700

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,7 +1,6 @@
 from agno.agent import Agent, RunResponse
 from agno.models.deepseek import DeepSeek
 
-api_key = "sk-087c2d0e8af14432bdb0e7d06a15ac78"
 agent = Agent(model=DeepSeek(id = "deepseek-reasoner"), markdown=True)
 
 agent.print_response("Share a 2 sentence horror story.")
